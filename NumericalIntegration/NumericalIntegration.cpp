fd2a11f3e2cbd72486d5b053bad60fe496d69140
thomasnorris <tom.norris135@gmail.com>
2018-01-26 14:00:57 -0500

    global values for limits; prints limits and steps at the end

--- NumericalIntegration/NumericalIntegration.cpp
+++ NumericalIntegration/NumericalIntegration.cpp
@@ -9,28 +9,27 @@ using namespace std;
 
 int _numSteps;
 double _total = 0;
+double _upperLimit = 0;
+double _lowerLimit = 0;
 
 void DoCalculations(double lower_limit, double step_size, int i);
 
 int main()
 {
-	double upperLimit;
-	double lowerLimit;
-
 	cout << "This program will approximate the value of the integral of Sin(x) given a lower limit, upper limit, and number of steps that should be used." << endl;
 	cout << "Please input only valid numbers." << endl << "" << endl;
 
 	cout << "Enter the lower limit: ";
-	cin >> lowerLimit;
+	cin >> _lowerLimit;
 	cout << "Enter the upper limit: ";
-	cin >> upperLimit;
+	cin >> _upperLimit;
 	cout << "Enter the number of steps to use: ";
 	cin >> _numSteps;
 	cout << endl << "A table of calculation data follows" << endl << endl;
 
-	double stepSize = (upperLimit - lowerLimit) / _numSteps;
+	double stepSize = (_upperLimit - _lowerLimit) / _numSteps;
 	
-	DoCalculations(lowerLimit, stepSize, 0);
+	DoCalculations(_lowerLimit, stepSize, 0);
 
 	return EXIT_SUCCESS;
 }
@@ -53,7 +52,7 @@ void DoCalculations(double a, double h, int count)
 		DoCalculations(b, h, ++count);
 	else
 	{
-		cout << endl << "Calculations completed." << endl << endl << "The end result is approximately: " << _total << endl << endl;
+		cout << endl << "Calculations completed." << endl << endl << "The value of the integration of Sin(x) with a lower limit of " << _lowerLimit << " and an upper limit of " << _upperLimit << " using " << _numSteps << " steps is approximately " << _total << endl << endl;
 		system("pause");
 	}
 }
