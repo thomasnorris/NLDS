f7eb5abaa2479f6b9aada6869521c39405575217
thomasnorris <tom.norris135@gmail.com>
2018-01-30 11:46:13 -0500

    switched out variables for a vector for simplification

--- NumericalIntegration/NumericalIntegration.cpp
+++ NumericalIntegration/NumericalIntegration.cpp
@@ -37,26 +37,26 @@ int main()
 
 void DoCalculationsAndPrintResults(double a, int count)
 {
-	// Variable names came from the handout
-	auto b = a + _stepSize;
-	auto firstPart = sin(a);
-	auto secondPart = sin(((2 * a) + b) / 3);
-	auto thirdPart = sin((a + (2 * b)) / 3);
-	auto fourthPart = sin(b);
-	auto value = ((b - a) / 8) * (firstPart + (3 * secondPart) + (3 * thirdPart) + fourthPart);
+	vector<double> calculations;
 
-	_total += value;
+	calculations.push_back(a);
+	calculations.push_back(a + _stepSize);
+	calculations.push_back(sin(a));
+	calculations.push_back(sin(((2 * a) + calculations[1]) / 3));
+	calculations.push_back(sin((a + (2 * calculations[1])) / 3));
+	calculations.push_back(sin(calculations[1]));
+	calculations.push_back(((calculations[1] - a) / 8) * (calculations[2] + (3 * calculations[3]) + (3 * calculations[4]) + calculations[5]));
 
-	FormatAndPrint(b);
-	FormatAndPrint(firstPart);
-	FormatAndPrint(secondPart);
-	FormatAndPrint(thirdPart);
-	FormatAndPrint(fourthPart);
-	FormatAndPrint(value);
+	_total += calculations[6];
+
+	for (auto it = calculations.begin(); it != calculations.end(); ++it) {
+		FormatAndPrint(*it);
+	}
+
 	cout << endl;
 	
 	if (count != _numSteps - 1)
-		DoCalculationsAndPrintResults(b, ++count);
+		DoCalculationsAndPrintResults(calculations[1], ++count);
 	else
 	{
 		cout << endl << "Calculations completed." << endl << endl << "The value of the integration of Sin(x) with a lower limit of " << _lowerLimit << " and an upper limit of " << _upperLimit << " using " << _numSteps << " step(s) is approximately " << _total << "." << endl << endl;
